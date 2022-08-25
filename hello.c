7f609115c3261f825b79c74ad81bf6e7ed9d098f
Jordyn Cheyney <jcheyney2l@huskers.unl.edu>
2022-08-25 19:39:59 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ *  Basic Hello world program
+ */
+
+
+int main(int argc, char **argv){
+    printf("Hello, I'm Jordyn Cheyney\n");
+    printf("Major Is Computer Science!\n");
+
+    return 0;
+}
